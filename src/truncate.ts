ff605d506502729d6318737c4c252339cd22b4fc
Robert Patan <robert@patan.dev>
2023-03-03 12:50:53 +0200

    1.0.5

--- src/truncate.ts
+++ src/truncate.ts
@@ -4,7 +4,7 @@ export async function truncateTables(dataSource: DataSource): Promise<void> {
   const entities = dataSource.entityMetadatas;
   for (const entity of entities) {
     const repository = dataSource.getRepository(entity.name);
-    await repository.query(`TRUNCATE TABLE \"${entity.tableName}\" CASCADE`);
+    await repository.query(`TRUNCATE TABLE \\"${entity.tableName}\\" CASCADE`);
   }
 }
 
